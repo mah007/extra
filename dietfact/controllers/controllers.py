4e02f94636512212464c1f89f782f9ac80f72520
Mahmoud <mah008@me.com>
2020-06-26 01:42:01 +0200

    fix what has to be fixed

--- dietfact/controllers/controllers.py
+++ dietfact/controllers/controllers.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class Dietfact(http.Controller):
-#     @http.route('/dietfact/dietfact/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Dietfact(http.Controller):
+    @http.route('/dietfact/dietfact/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/dietfact/dietfact/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('dietfact.listing', {
-#             'root': '/dietfact/dietfact',
-#             'objects': http.request.env['dietfact.dietfact'].search([]),
-#         })
+    @http.route('/dietfact/dietfact/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('dietfact.listing', {
+            'root': '/dietfact/dietfact',
+            'objects': http.request.env['dietfact.dietfact'].search([]),
+        })
 
-#     @http.route('/dietfact/dietfact/objects/<model("dietfact.dietfact"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('dietfact.object', {
-#             'object': obj
-#         })
+    @http.route('/dietfact/dietfact/objects/<model("dietfact.dietfact"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('dietfact.object', {
+            'object': obj
+        })
